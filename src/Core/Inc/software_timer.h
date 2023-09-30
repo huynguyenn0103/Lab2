daa664099313a8196d386fba8a099a63e53a4b33
huynguyenn0103 <huynguyenn0103@gmail.com>
2023-09-30 15:22:53 +0700

    first commit for main branch

---
+++ src/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 30, 2023
+ *      Author: HP
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
