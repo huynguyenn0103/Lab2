9ba07d1f18090e2b0581aeefd2dd18e88a030312
huynguyenn0103 <huynguyenn0103@gmail.com>
2023-09-30 23:37:10 +0700

    second commit for branch ex6

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -118,6 +118,7 @@ int main(void)
 	  	  setTimer2(100);
 	  }
 	  if (timer3_flag == 1){
+		  second++;
 		  if(second >= 60){
 			  second = 0;
 			  minute++;
@@ -129,7 +130,6 @@ int main(void)
 		  if(hour >= 24){
 			  hour = 0;
 		  }
-		  second++;
 		  updateClockBuffer(hour, minute);
 		  setTimer3(100);
 	  }
